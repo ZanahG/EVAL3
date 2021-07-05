e49efa7396252a3a99965c47dd56857d065a2333
ZanahG <dsagredo.cl@gmail.com>
2021-07-05 13:05:50 -0400

    token

--- webservice_mascota/views.py
+++ webservice_mascota/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
 from appPeludo.models import Mascotas
 from .serializers import MascotasSerializer
-
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 @csrf_exempt
 @api_view(['GET','POST'])
+@permission_classes((IsAuthenticated,))
 
 def lista_mascotas(request):
 
@@ -27,6 +29,7 @@ def lista_mascotas(request):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET','PUT','DELETE'])
+@permission_classes((IsAuthenticated,))
 
 def detalle_mascota(request, codigo):
     
