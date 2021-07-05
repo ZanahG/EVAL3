9844d92b9bc6d13957211eb972030b566cfbb0c2
ZanahG <dsagredo.cl@gmail.com>
2021-07-05 12:35:49 -0400

    authtoken

--- fundacionPeludo/settings.py
+++ fundacionPeludo/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'appPeludo',
     'rest_framework',
-    'webservice_mascota'
+    'webservice_mascota',
+    'rest_framework.authtoken',
 
 
 ]
