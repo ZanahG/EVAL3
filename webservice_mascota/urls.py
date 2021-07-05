8a9aa8822a49a3c5c9246c44afc232fe286f3da1
ZanahG <dsagredo.cl@gmail.com>
2021-07-05 12:50:00 -0400

    token login

--- webservice_mascota/urls.py
+++ webservice_mascota/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from django.urls import path
 from webservice_mascota.views import lista_mascotas, detalle_mascota
+from webservice_mascota.viewsLogin import login
 
 urlpatterns = [
     path('lista_mascotas',lista_mascotas, name="lista_mascotas"),
     path('detalle_mascota/<str:codigo>/', detalle_mascota, name="detalle_mascotas"),
+    path('login', login, name="login"),
 ]
 
 
